851fe0e620ce3668f73bb42daa59241c5ae15391
anangyoga <aharwinto@gmail.com>
2022-06-06 18:47:00 +0700

    quick review: typescript 101

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
 import React from "react";
 import "./App.css";
 
+let name: string;
+name = "Lola";
+
+// Alias: Type
+type Person = {
+  name: string;
+  age: number;
+  isMarried?: boolean;
+};
+
+//Alias: Interface
+interface Human {
+  race: string;
+  color: string;
+  blueEyes?: boolean;
+}
+
+let myNeighbour: Person = {
+  name: "Baja",
+  age: 43,
+};
+
+let indonesian: Human = {
+  race: "asian",
+  color: "yellow",
+  blueEyes: false,
+};
+
+// this is how we assign array of Object
+let arrayOfObj: Person[];
+
+// UNION type: we use it to declare two different types
+let age: number | string;
+let umur: number | string;
+
+age = "Twenty Two";
+umur = 55;
+
+// Function type
+let printName: (name: string) => void;
+
+// Unknown type
+let printAddress: unknown;
+
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <p>Hello {name}</p>
     </div>
   );
 }
