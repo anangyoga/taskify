0a781829671379ca80c4ec0b7e281dc7b5cb74dc
anangyoga <aharwinto@gmail.com>
2022-06-08 22:48:52 +0700

    feat: create input todo

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form action="" className="input">
-      <input className="input__box" type="input" placeholder="Enter a task" />
+    <form action="" className="input" onSubmit={handleAdd}>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} className="input__box" type="input" placeholder="Enter a task" />
       <button className="input__submit" type="submit">
         Go
       </button>
